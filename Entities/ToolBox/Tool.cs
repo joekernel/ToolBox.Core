9bf7db8e535d85bb93c798b1515e48e846263a60
Mateusz Pasterny <mat.pasterny@gmail.com>
2019-01-15 19:24:55 +0100

    Guid Id has private setter now, Tool class implemented ITool interface

--- Entities/ToolBox/Tool.cs
+++ Entities/ToolBox/Tool.cs
@@ -1,13 +1,14 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using System;
+using ToolBox.Core.Interfaces;
 using ToolBox.Infrastructure.Interfaces;
 
 namespace ToolBox.Core.Entities
 {
-    public class Tool : IMongoEntity
+    public class Tool : ITool,IMongoEntity
     {
         [BsonId]
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
         public string Name { get; private set; }
         public double Weight { get; private set; }
         public double Capacity { get; private set; }
