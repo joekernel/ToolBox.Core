7343d1f5cdf8cc2621ac46b6465c51e2080f7a7c
Mateusz Pasterny <mat.pasterny@gmail.com>
2019-01-15 19:22:04 +0100

    IContainer interface add as new file

---
+++ Interfaces/IContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToolBox.Core.Entities;
+
+namespace ToolBox.Core.Interfaces
+{
+    public interface IContainer
+    {
+        Guid Id { get; }
+        double ContainerMaxCapacity { get; }
+        double ContainerMaxWeight { get; }
+        double ContainerCapacity { get; }
+        double ContainerWeight { get; }
+        List<ContainerItem> Items { get; }
+        void AddItem(Tool item, int quantity = 1);
+    }
+}
