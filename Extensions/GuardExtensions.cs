00c01c869fe79f95127350d033bbf219c3b122d0
Mateusz Pasterny <mat.pasterny@gmail.com>
2019-01-15 19:18:33 +0100

    ContainerWeightOverload adjusted to concrete container exception type

--- Extensions/GuardExtensions.cs
+++ Extensions/GuardExtensions.cs
@@ -1,24 +1,25 @@
 ﻿using MongoDB.Bson;
 using System;
 using ToolBox.Core.Entities;
+using ToolBox.Core.Exceptions;
 
 namespace Ardalis.GuardClauses
 {
     public static class GuardExtensions
     {
-        public static void ContainerCapacityOverload(this IGuardClause guardClause, double capacity,double maxCapacity)
+        public static void ContainerCapacityOverload(this IGuardClause guardClause, double capacity, double maxCapacity)
         {
             if (maxCapacity < capacity)
                 throw new Exception($"The container capacity is overloaded, you can't add this item.");
         }
 
-        public static void ContainerWeightOverload(this IGuardClause guardClause,double weight, double maxWeight)
+        public static void ContainerWeightOverload(this IGuardClause guardClause, Container container, double weight)
         {
-            if (maxWeight < weight)
-                throw new Exception($"The container weight is overloaded, you can't add this item.");
+            if (container.ContainerMaxWeight < container.ContainerWeight+weight)
+                throw new ContainerWeightOverloadException(container.Id);
         }
 
-        public static void ContainerNotFound(this IGuardClause guardClause, Container container,Guid containerId)
+        public static void ContainerNotFound(this IGuardClause guardClause, Container container, Guid containerId)
         {
             if (container == null)
                 throw new Exception($"Container not found");
