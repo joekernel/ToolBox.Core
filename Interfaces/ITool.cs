1d5f0f2eb0ab04d77d6c15ebcc4367911a7f6e46
Mateusz Pasterny <mat.pasterny@gmail.com>
2019-01-15 19:25:17 +0100

    ITool interface created

---
+++ Interfaces/ITool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ToolBox.Core.Interfaces
+{
+    public interface ITool
+    {
+        Guid Id { get; }
+        string Name { get; }
+        double Weight { get; }
+        double Capacity { get; }
+        string Category { get; }
+    }
+}
