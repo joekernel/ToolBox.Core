7a4c2120c407df340cc9af3b922976125ef42416
Mateusz Pasterny <mat.pasterny@gmail.com>
2019-01-15 19:19:16 +0100

    Created concrete container exception for weight overload

---
+++ Exceptions/ContainerWeightOverloadException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ToolBox.Core.Exceptions
+{
+    public class ContainerWeightOverloadException : Exception
+    {
+        public ContainerWeightOverloadException(Guid containerId):base(containerId.ToString())
+        {
+        }
+
+        public ContainerWeightOverloadException(string message) : base(message)
+        {
+        }
+
+        public ContainerWeightOverloadException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ContainerWeightOverloadException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
